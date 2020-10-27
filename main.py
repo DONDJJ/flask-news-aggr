647a724d91c0b6f8b1023abab4d890e03f4da3e9
DONDJ <artem.alex.alekseev@mail.ru>
2020-10-27 19:24:10 +0300

    4 try

--- main.py
+++ main.py
@@ -5,17 +5,18 @@ from flask import Flask
 from multiprocessing import Process
 
 my_app=app
-if __name__ == '__main__':
-    func_list = [news_request_forDB.database_update, Flask.run]
-    proc_list = []
-    for i in func_list:
-        if i is Flask.run:
-            p = Process(target=app.run())
-        else:
-            p = Process(target=i)
-        p.start()
-        proc_list.append(p)
 
-    for i in proc_list:
-        i.join()
+# if __name__ == '__main__':
+#     func_list = [news_request_forDB.database_update, Flask.run]
+#     proc_list = []
+#     for i in func_list:
+#         if i is Flask.run:
+#             p = Process(target=app.run())
+#         else:
+#             p = Process(target=i)
+#         p.start()
+#         proc_list.append(p)
+#
+#     for i in proc_list:
+#         i.join()
 
